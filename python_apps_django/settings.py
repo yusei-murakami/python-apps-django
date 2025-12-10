cb38cc646a229f0779a9bd77eeb15990742a03b3
yusei-murakami <yusei0528358@gmail.com>
2025-12-10 11:00:48 +0900

    個人開発

--- python_apps_django/settings.py
+++ python_apps_django/settings.py
@@ -157,10 +157,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', '358'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
     }
 }
 
